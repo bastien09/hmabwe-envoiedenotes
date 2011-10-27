4547a5f9623ba281a1029deeeccc01668f787c0d
iliasse.hassala@gmail.com <iliasse.hassala@gmail.com@300ebd64-35fe-233c-d2ed-9c1503628577>
2011-10-27 17:16:55 +0000

    l'import-export excel est en version rc

--- trunk/hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/controleur/es/tableur/TestExportExcel.java
+++ trunk/hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/controleur/es/tableur/TestExportExcel.java
@@ -1,10 +1,16 @@
 package fr.unice.hmabwe.controleur.es.tableur;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+import fr.unice.hmabwe.controleur.bd.dao.DaoException;
+import fr.unice.hmabwe.controleur.bd.dao.jpa.JpaDaoEtudiant;
+import fr.unice.hmabwe.modele.Cours;
 import fr.unice.hmabwe.modele.Etudiant;
 import fr.unice.hmabwe.modele.Filiere;
+import fr.unice.hmabwe.modele.Inscription;
 
 public class TestExportExcel {
 
@@ -14,17 +20,26 @@ public class TestExportExcel {
 	public static void main(String[] args) {
 		
 		ExportExcel eex = new ExportExcel();
+		Cours cours = new Cours("POO");
 		
-		HashMap<Etudiant, String> ed = new HashMap<Etudiant, String>();
+		int annee = 2010;
 		
-		Filiere f1 = new Filiere("M1 info");
-		Etudiant e1 = new Etudiant("hi911803", "hassala", "iliasse", "hi911803@gmail.com","L3I",f1);
-		Etudiant e2 = new Etudiant("av702264", "actis", "virginie", "vactis@polytech.unice.fr","SI3",f1);
+		JpaDaoEtudiant etus = new JpaDaoEtudiant();
 		
-		ed.put(e1, "12");
-		ed.put(e2, "20");
+		HashMap<Etudiant, String> ed = null;
+		/*
+		try {
+			
+			/*appeler une méthode qui renverra une HashMap<Etudiant, String> qui devra contenir :
+			 * - les étudiants inscrits au cours c l'année a (c et a seront passés en paramètres);
+			 * - leurs moyenne pour cette inscription sous forme de String.*/
+			
+			//ed = /*******/
+		/*} catch (DaoException e) {
+			e.printStackTrace();
+		}*/
 		
-		eex.createXls(ed);
+		eex.createXls(ed, cours, annee);
 
 	}
 
