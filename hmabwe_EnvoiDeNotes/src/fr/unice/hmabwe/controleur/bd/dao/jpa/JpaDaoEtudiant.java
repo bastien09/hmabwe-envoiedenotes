d9f060dfbd838d4cb95b493653ee0a85afa9a57c
swanmanson@hotmail.fr <swanmanson@hotmail.fr@300ebd64-35fe-233c-d2ed-9c1503628577>
2011-10-28 09:34:37 +0000

    code creux de certaine methode metier

--- hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/controleur/bd/dao/jpa/JpaDaoEtudiant.java
+++ hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/controleur/bd/dao/jpa/JpaDaoEtudiant.java
@@ -3,11 +3,14 @@
  */
 package fr.unice.hmabwe.controleur.bd.dao.jpa;
 
+import java.util.HashMap;
+
 import javax.persistence.Embeddable;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import fr.unice.hmabwe.controleur.bd.dao.DaoEtudiant;
+import fr.unice.hmabwe.modele.Cours;
 import fr.unice.hmabwe.modele.Etudiant;
 
 /**
@@ -25,6 +28,8 @@ implements DaoEtudiant{
 	private String RequeteEtaitInscrit = "select e from Etudiant as e where " +
 			"e.nom = :etuNom and " +
 			"e.";
+	//TODO chercher la requete
+	private String RequeteListeInscrit = "";
 
 	/**
 	 * le code métier va ici
@@ -36,5 +41,10 @@ implements DaoEtudiant{
 		Query q = null;
 		e.createQuery(RequeteEtaitInscrit);
 		return true;
+	}
+	//TODO methode à faire
+	public HashMap<Etudiant, String> listeInscrit(Cours cours, Integer annee){
+		
+		return null;
 	}
 }
