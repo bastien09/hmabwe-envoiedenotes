e780aa9a6a11289632927493940c73994365b1e9
iliasse.hassala@gmail.com <iliasse.hassala@gmail.com@300ebd64-35fe-233c-d2ed-9c1503628577>
2011-10-31 14:43:44 +0000

    Javadoc + amélioration du TestMail

--- hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/controleur/es/mail/TestSendMailSSL.java
+++ hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/controleur/es/mail/TestSendMailSSL.java
@@ -6,20 +6,21 @@ public class TestSendMailSSL {
 
 		MailSSL mail = new MailSSL("login@etu.unice.fr", "password",
 				"smtp.unice.fr", 465);
+
 		// on envoi 3 mails contenant le texte passé en paramètre avec les tags,
 		// à l'adresse situé dans le champ "to" avec le "subject" choisit. On
 		// peut paramétré l'envoi avec un e.getMail() dans le champ "to"
+
 		for (int i = 0; i < 3; i++) {
 
+			// méthode qui remplace les tags par leur valeur.On passe ici en dur
+			// le nom et le prénom qui devront remplacer les tags présents dans
+			// le texte.
 			mail.setTags("Anthony", "Biga", "", "", "", "", "", "");
+
 			mail.SendMail("from@etu.unice.fr", "to@etu.unice.fr", "Subject",
 					"salut #{nom} #{prenom}");
 		}
-		/*
-		 * mail.setTags("Hassala", "Iliasse", "", "", "", "", "", "");
-		 * mail.SendMail("frem", "to@etu.unice.fr", "Subject",
-		 * "salut #{nom} #{prenom}");
-		 */
 
 	}
 
