698beabe212a11724bd169623cb41f8fb8e8e5e7
Bastien.Auda@gmail.com <Bastien.Auda@gmail.com@300ebd64-35fe-233c-d2ed-9c1503628577>
2011-11-04 18:30:51 +0000

    La fenêtre principale semble fonctionnelle, j'attend les fonctions des DAO pour afficher les données. La fenêtre de préférences reste à faire.

--- trunk/hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/vue/FenetrePrincipale.java
+++ trunk/hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/vue/FenetrePrincipale.java
@@ -1,14 +1,18 @@
 package fr.unice.hmabwe.vue;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JSplitPane;
 
 import fr.unice.hmabwe.controleur.bd.Connexion;
@@ -24,155 +28,217 @@ import fr.unice.hmabwe.controleur.bd.dao.DaoFabrique;
  *
  */
 public class FenetrePrincipale extends JFrame {
-	private Connexion conn;
-	
+	private DaoFabrique df;
+
+	private MenuListener l = new MenuListener();
+
 	// La barre des menus
 	private JMenuBar menu = new JMenuBar();
-	
+
 	// Les menus
 	private JMenu fichier = new JMenu("Fichier");
 	private JMenu edition = new JMenu("Édition");
 	private JMenu presentation = new JMenu("Présentation");
 	private JMenu aide = new JMenu("Aide");
-	
+
 	// Les items du menu fichier
 	private JMenu ajouter = new JMenu("Ajouter");
 	private JMenuItem addEtudiant = new JMenuItem("un étudiant");
 	private JMenuItem addEnseignant = new JMenuItem("un enseignant");
 	private JMenuItem addCours = new JMenuItem("un cours");
 	private JMenuItem addFiliere = new JMenuItem("une filière");
-	
+
 	private JMenuItem importFromXls = new JMenuItem("Importer depuis Excel", new ImageIcon(this.getClass().getResource("/resource/import.png")));
 	private JMenuItem exportToXls = new JMenuItem("Exporter vers Excel", new ImageIcon(this.getClass().getResource("/resource/export.png")));
-	
+
 	private JMenuItem email = new JMenuItem("Envoyer les notes par e-mail", new ImageIcon(this.getClass().getResource("/resource/mail--arrow.png")));
-	
+
 	private JMenuItem quitter = new JMenuItem("Quitter", new ImageIcon(this.getClass().getResource("/resource/cross-button.png")));
-	
+
 	// Les items du menu edition
 	private JMenuItem copier = new JMenuItem("Copier", new ImageIcon(this.getClass().getResource("/resource/document-copy.png")));
 	private JMenuItem coller = new JMenuItem("Coller", new ImageIcon(this.getClass().getResource("/resource/clipboard-paste-document-text.png")));
 	private JMenuItem selectAll = new JMenuItem("Tout sélectionner");
-	
+
 	// Les items du menu d'aide
 	private JMenuItem about = new JMenuItem("À propos", new ImageIcon(this.getClass().getResource("/resource/information-frame.png")));
 	private JMenuItem web = new JMenuItem("Site web du projet", new ImageIcon(this.getClass().getResource("/resource/question-frame.png")));
-	
-	// les composants de la fenêtre
-	private PanneauGauchePrincipal panneauGauche = new PanneauGauchePrincipal(conn);
-	private PanneauListeEtudiants panneauDroite = new PanneauListeEtudiants(conn);
-	private JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panneauGauche, panneauDroite);
-	
-		
 
-	public FenetrePrincipale(Connexion conn) {
-		this.conn = conn;
-		
+	// les composants de la fenêtre
+	private PanneauGauchePrincipal panneauGauche;
+	private PanneauListeEtudiants panneauDroite;
+	private JSplitPane splitPane;
+
+
+
+	public FenetrePrincipale(DaoFabrique df) {
+		this.df = df;
+		panneauGauche = new PanneauGauchePrincipal(df);
+		panneauDroite  = new PanneauListeEtudiants(df);
+		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panneauGauche, panneauDroite);
+
 		this.setTitle("Envoi de note - hmabwe");
 		this.setSize(800, 600);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLayout(new BorderLayout());
-		
+
 		addMenuBar();
-		
+
 		this.add(splitPane);
-		
+
 		this.setVisible(true);
+
 	}
-	
+
 	/**
 	 * Ajoute une barre des menus à la fenêtre.
 	 */
 	public void addMenuBar() {
-				
+
 		ajouter.add(addEtudiant);
+		addEtudiant.addActionListener(l);
 		ajouter.add(addEnseignant);
+		addEnseignant.addActionListener(l);
 		ajouter.add(addCours);
+		addCours.addActionListener(l);
 		ajouter.add(addFiliere);
-		
+		addFiliere.addActionListener(l);
+
 		ajouter.setIcon(new ImageIcon(this.getClass().getResource("/resource/plus.png")));
 		fichier.add(ajouter);
 		fichier.addSeparator();
 		fichier.add(importFromXls);
+		importFromXls.addActionListener(l);
 		fichier.add(exportToXls);
+		exportToXls.addActionListener(l);
 		fichier.add(email);
+		email.addActionListener(l);
 		fichier.addSeparator();
 		fichier.add(quitter);
-		
+		quitter.addActionListener(l);
+
 		edition.add(copier);
+		copier.addActionListener(l);
 		edition.add(coller);
+		coller.addActionListener(l);
 		edition.add(selectAll);
-		
+		selectAll.addActionListener(l);
+
 		aide.add(about);
+		about.addActionListener(l);
 		aide.add(web);
-		
+		web.addActionListener(l);
+
 		menu.add(fichier);
 		menu.add(edition);
 		//menu.add(presentation);
 		menu.add(aide);
-		
+
 		this.setJMenuBar(menu);
-		
+
 		this.addWindowListener(new FenetrePrincipaleListener());
 	}
-	
+
+	/**
+	 * Ferme la fenêtre en se déconnectant de la base proprement. 
+	 */
+	private void close() {
+		try {
+			df.getConnexion().fermer();
+		} catch (DaoException e1) {
+			e1.printStackTrace();
+			System.out.println("La connexion n'a pas été fermée correctement, des données ont pu être corrompue.");
+		}
+		System.exit(0);
+	}
+
 	public static void main(String[] args) {
-		
+
 		DaoFabrique.setTypeDao(DaoFabrique.TypeFabrique.JPA);
 
 		DaoFabrique df = DaoFabrique.getDaoFabrique();
 
-		Connexion conn = df.getConnexion();
+		new FenetrePrincipale(df);
 
-		new FenetrePrincipale(conn);
-		
 	}
+
 	
+	/*
+	 * Le listener de la fenêtre
+	 */
 	private class FenetrePrincipaleListener implements WindowListener {
 
 		@Override
 		public void windowActivated(WindowEvent e) {
-						
+
 		}
 
 		@Override
 		public void windowClosed(WindowEvent e) {
-			
+
 		}
 
 		@Override
 		public void windowClosing(WindowEvent e) {
-			
-			try {
-				conn.fermer();
-			} catch (DaoException e1) {
-				e1.printStackTrace();
-				System.out.println("La connexion n'a pas été fermée correctement, des données ont pu être corrompue.");
-			}
-			
+
+			((FenetrePrincipale) e.getSource()).close();
+
 		}
 
 		@Override
 		public void windowDeactivated(WindowEvent e) {
-			
+
 		}
 
 		@Override
 		public void windowDeiconified(WindowEvent e) {
-			
+
 		}
 
 		@Override
 		public void windowIconified(WindowEvent e) {
-			
+
 		}
 
 		@Override
 		public void windowOpened(WindowEvent e) {
-			
+
 		}
-		
+
 	}
-	
+
+	/*
+	 * Le listener de la barre de menu
+	 */
+	private class MenuListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			Object source = e.getSource();
+			
+			if(source.equals(quitter)) {
+				close();
+			}
+			if(source.equals(web)) {
+				String url = "http://code.google.com/p/hmabwe-envoiedenotes/";
+				try {
+					java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(FenetrePrincipale.this,
+						    "Impossible de lancer votre navigateur web.",
+						    "Erreur",
+						    JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			if(source.equals(about)) {
+				new FenetreAPropos();
+			}
+
+
+		}
+
+	}
+
 }
--- trunk/hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/vue/PanneauGauchePrincipal.java
+++ trunk/hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/vue/PanneauGauchePrincipal.java
@@ -2,59 +2,169 @@ package fr.unice.hmabwe.vue;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import fr.unice.hmabwe.controleur.bd.Connexion;
+import fr.unice.hmabwe.controleur.bd.dao.DaoCours;
+import fr.unice.hmabwe.controleur.bd.dao.DaoException;
+import fr.unice.hmabwe.controleur.bd.dao.DaoFabrique;
+import fr.unice.hmabwe.controleur.bd.dao.DaoFiliere;
+import fr.unice.hmabwe.modele.Cours;
+import fr.unice.hmabwe.modele.Filiere;
 
 /**
  * 
  * @author Bastien Auda
  * 
- * Le JPanel contenant la 
+ * Le JPanel contenant la liste des filières et cours.
  *
  */
 
 public class PanneauGauchePrincipal extends JPanel {
-	
+
+	private DaoFabrique df;
 	private Connexion conn;
-	
+	private DaoFiliere daoFiliere;
+	private DaoCours daoCours;
+
+	private EcouteurGauche l = new EcouteurGauche();
+
 	private JComboBox choix = new JComboBox();
 	private JList liste = new JList();
-	
+
 	private JPanel boutons = new JPanel();
 	private JButton stats = new JButton(new ImageIcon(this.getClass().getResource("/resource/chart.png")));
 	private JButton edit = new JButton(new ImageIcon(this.getClass().getResource("/resource/pencil.png")));
 	private JButton remove = new JButton(new ImageIcon(this.getClass().getResource("/resource/minus-circle.png")));
 	private JButton add = new JButton(new ImageIcon(this.getClass().getResource("/resource/plus-circle.png")));
-	
-	public PanneauGauchePrincipal(Connexion conn) {
-		
-		this.conn = conn;
-		
+
+	public PanneauGauchePrincipal(DaoFabrique df) {
+		this.df = df;
+		conn = df.getConnexion();
+		daoFiliere = df.getDaoFiliere();
+		daoCours = df.getDaoCours();
+
 		this.setLayout(new BorderLayout());
-		
+
 		choix.addItem("Filière");
 		choix.addItem("Cours");
-		
+		choix.addActionListener(l);
+
 		this.add(choix, BorderLayout.NORTH);
-		this.add(liste, BorderLayout.CENTER);
-		Object[] tab = { 1, 2, 3, 5};
-		liste.setListData(tab);
-		
+		this.add(new JScrollPane(liste), BorderLayout.CENTER);
+		refreshList();
+
+
 		boutons.add(stats);
+		stats.addActionListener(l);
 		boutons.add(edit);
+		edit.addActionListener(l);
 		boutons.add(remove);
+		remove.addActionListener(l);
 		boutons.add(add);
-		
+		add.addActionListener(l);
+
 		this.add(boutons, BorderLayout.SOUTH);
-		
+
 	}
-	
+
+	/**
+	 * rafraichit la liste des filières et cours
+	 */
+	public void refreshList() {
+		try {
+			conn.beginTransaction();
+			if(choix.getSelectedItem().equals("Filière")) {
+				//TODO liste.setListData(daoFiliere.getAllFilieres().toArray());
+			} else {
+				//TODO liste.setListData(daoCours.getAllCours().toArray());
+			}
+			liste.setSelectedIndex(0);
+			conn.commitTransaction();
+		} catch (DaoException e) {
+			//TODO error message
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Le listener des composants du panneau
+	 */
+
+	private class EcouteurGauche implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object source = e.getSource();
+			if(source.equals(choix)) {
+				refreshList();
+			}
+			if(source.equals(add)) {
+				if(choix.getSelectedItem().equals("Cours")) {
+					//TODO ajout cours
+					new FenetreAjoutCours("", 600, 600);
+				} else {
+					//TODO ajout filiere
+					new FenetreAjoutFiliere("", 600, 600);
+				}
+			}
+			if(source.equals(remove)) {
+				String objetASupprimer;
+				try {
+					conn.beginTransaction();
+					if(choix.getSelectedItem().equals("Cours")) {
+						Cours c = (Cours) liste.getSelectedValue();
+						objetASupprimer = "le cours " + c.getNom();
+						daoCours.delete(c);
+					} else {
+						Filiere f = (Filiere) liste.getSelectedValue();
+						objetASupprimer = "la filière " + f.getNom();
+						daoFiliere.delete(f);
+					}
+					int reponse = JOptionPane.showConfirmDialog(PanneauGauchePrincipal.this.getParent(), "Êtes vous certain de vouloir supprimer " +
+							objetASupprimer + " ?");
+					if(reponse == JOptionPane.OK_OPTION) {
+						conn.commitTransaction();
+					} else {
+						conn.rollbackTransaction();
+					}
+				} catch (Exception ex) {
+					// TODO: handle exception
+				}
+			}
+			if(source.equals(edit)) {
+				if(choix.getSelectedItem().equals("Cours")) {
+					Cours c = (Cours) liste.getSelectedValue();
+					//TODO edit cours
+					new FenetreAjoutCours("", 600, 600);
+				} else {
+					Filiere f = (Filiere) liste.getSelectedValue();
+					//TODO edit filiere
+					new FenetreAjoutFiliere("", 600, 600);
+				}
+			}
+			if(source.equals(stats)) {
+				if(choix.getSelectedItem().equals("Cours")) {
+					Cours c = (Cours) liste.getSelectedValue();
+					//TODO stats cours
+				} else {
+					Filiere f = (Filiere) liste.getSelectedValue();
+					//TODO stats filiere
+				}
+			}
+		}
+
+
+	}
 
 }
