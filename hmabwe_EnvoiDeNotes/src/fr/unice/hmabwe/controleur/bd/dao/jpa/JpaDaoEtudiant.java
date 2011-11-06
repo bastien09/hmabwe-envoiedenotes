68b42e7b406cf9cd14b18a3b0f6c445279f5cc62
milkyboi@gmail.com <milkyboi@gmail.com@300ebd64-35fe-233c-d2ed-9c1503628577>
2011-11-06 18:55:53 +0000

    création méthode getMoyenne de DaoEtudiant (work in progress) car j'en ai besoin dans getMoyenne d'une filiere

--- hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/controleur/bd/dao/jpa/JpaDaoEtudiant.java
+++ hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/controleur/bd/dao/jpa/JpaDaoEtudiant.java
@@ -3,6 +3,7 @@
  */
 package fr.unice.hmabwe.controleur.bd.dao.jpa;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.HashMap;
 
@@ -10,8 +11,11 @@ import javax.persistence.Embeddable;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import fr.unice.hmabwe.controleur.bd.dao.DaoEtudiant;
+import fr.unice.hmabwe.modele.Coefficient;
 import fr.unice.hmabwe.modele.Cours;
 import fr.unice.hmabwe.modele.Etudiant;
+import fr.unice.hmabwe.modele.Filiere;
+import fr.unice.hmabwe.modele.Inscription;
 
 /**
  * @author Paraita Wohler
@@ -80,5 +84,28 @@ implements DaoEtudiant{
 		else {
 			return res.get(0);
 		}
+	}
+	
+	
+	/**
+	 * @see fr.unice.hmabwe.controleur.dao.DaoEtudiant#getMoyenne()
+	 */
+	//TODO finir la méthode !
+	public double getMoyenne(String numEtu) {
+		EntityManager em = getEntityManager();
+		Query q = em.createQuery("select c from Coefficient c where c.filiere_id = :id");
+		Etudiant e = findByNumeroEtudiant(numEtu);
+		Collection<Inscription> l_inscr = e.getInscriptions();
+		Filiere f = e.getFiliere();
+		q.setParameter("id", f.getId());
+		double somme = 0;
+		List<Coefficient> l_coeffs = q.getResultList();
+		for (Coefficient coefficient : l_coeffs) {
+		}
+		
+		
+		
+		
+		return 0;
 	}
 }
