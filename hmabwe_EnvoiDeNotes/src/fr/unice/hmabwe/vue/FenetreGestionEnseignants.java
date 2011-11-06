d11c4e7018524a1ea1cb5191fdf4f2c74e8575b3
agora.univers <agora.univers@300ebd64-35fe-233c-d2ed-9c1503628577>
2011-11-06 21:12:35 +0000

    Mise en place des ecouteurs Enseignant

--- hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/vue/FenetreGestionEnseignants.java
+++ hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/vue/FenetreGestionEnseignants.java
@@ -22,7 +22,10 @@ import javax.swing.JSplitPane;
 import javax.swing.JTextField;
 
 import fr.unice.hmabwe.controleur.bd.dao.DaoEnseignant;
+import fr.unice.hmabwe.controleur.bd.dao.DaoException;
 import fr.unice.hmabwe.controleur.bd.dao.DaoFabrique;
+import fr.unice.hmabwe.modele.Enseignant;
+import fr.unice.hmabwe.modele.Filiere;
 
 /**
  * 
@@ -34,14 +37,31 @@ import fr.unice.hmabwe.controleur.bd.dao.DaoFabrique;
 
 public class FenetreGestionEnseignants extends FenetreCommune{
 	
-	PanelGestionEnseignant panelEnseignant;
+	public PanelGestionEnseignant panelEnseignant;
+	
+	public DaoEnseignant daoEnseignant;
+	
+	public EcouteurEnseignant l;
 	
 	public FenetreGestionEnseignants(DaoFabrique df) {
 		super("Gestion des enseignants", 500, 500, df);
 		
-		DaoEnseignant daoEnseignant = df.getDaoEnseignant();
+		daoEnseignant = df.getDaoEnseignant();
 		
 		panelEnseignant = new PanelGestionEnseignant();
+		
+		l = new EcouteurEnseignant();
+		
+		boutonOK.addMouseListener(l);
+		boutonAnnuler.addMouseListener(l);
+		panelEnseignant.bAjout1.addMouseListener(l);
+		panelEnseignant.bAjout2.addMouseListener(l);
+		panelEnseignant.bModif1.addMouseListener(l);
+		panelEnseignant.bModif2.addMouseListener(l);
+		panelEnseignant.bMoins.addMouseListener(l);
+		panelEnseignant.bPlus.addMouseListener(l);
+		panelEnseignant.bSuppress1.addMouseListener(l);
+		panelEnseignant.bSuppress2.addMouseListener(l);
 		
 		this.setResizable(false);
 		this.container.add(panelEnseignant.getPanelPrincipal(), BorderLayout.CENTER);
@@ -65,31 +85,79 @@ public class FenetreGestionEnseignants extends FenetreCommune{
 				if(boutSelected.equals(panelEnseignant.bAjout2)){//Si on veut ajouter un nouveau cours
 					new FenetreAjoutCours(df);
 				}
+				else{
+					if(boutSelected.equals(boutonOK)){//Si on clique sur le boutonOK
+						Enseignant e = new Enseignant(panelEnseignant.getNom(), panelEnseignant.getPrenom(), panelEnseignant.getEmail());
+						
+						e.addFiliere(panelEnseignant.getFiliereSelect());
+						e.addCours(panelEnseignant.getCoursSelect());
+						
+						
+						try {
+							conn.beginTransaction();
+							daoEnseignant.create(e);
+							conn.commitTransaction();
+							
+						}
+						catch (DaoException e1) {
+							try {
+								conn.rollbackTransaction();
+							} catch (DaoException e2) {
+								
+								e2.printStackTrace();
+							}
+							e1.printStackTrace();
+						}
+						finally {
+							if(conn.estOuverte()) {
+								try {
+									conn.fermer();
+									//TODO Faire disparaitre la fenetre a la fin de la transaction
+								}
+								catch(DaoException e3) {
+									e3.printStackTrace();
+								}
+							}
+						}
+						
+					}
+					else{
+						if(boutSelected.equals(boutonAnnuler)){
+							//TODO Fermer la fenetre
+						}
+						
+						else{
+							if(boutSelected.equals(panelEnseignant.bMoins)){
+								//TODO methode de suppression...
+							}
+						}
+					}
+				}
 			}
 				
 		}
 
 		@Override
 		public void mouseEntered(MouseEvent arg0) {
-			// TODO Auto-generated method stub
+			
 			
 		}
 
 		@Override
 		public void mouseExited(MouseEvent arg0) {
-			// TODO Auto-generated method stub
+			
 			
 		}
 
 		@Override
 		public void mousePressed(MouseEvent arg0) {
-			// TODO Auto-generated method stub
+			
 			
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent arg0) {
-			// TODO Auto-generated method stub
+			
 			
 		}
 		
