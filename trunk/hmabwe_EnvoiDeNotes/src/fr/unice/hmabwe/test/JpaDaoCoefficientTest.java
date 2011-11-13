bdb085ffee9e210560b5de52ff0bb28c7aa1778d
nuonchanndarong@gmail.com <nuonchanndarong@gmail.com@300ebd64-35fe-233c-d2ed-9c1503628577>
2011-11-13 22:28:20 +0000

    add JpaDaoCoefficientTest.java and JpaDaoEtudiantTest.java

---
+++ trunk/hmabwe_EnvoiDeNotes/src/fr/unice/hmabwe/test/JpaDaoCoefficientTest.java
@@ -0,0 +1,45 @@
+package fr.unice.hmabwe.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class JpaDaoCoefficientTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Ignore
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+}
